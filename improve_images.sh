9f1fac390004e5f2c5cf475d5b43b08ef5e05338
Raul Burgos Murray <specializeddevel@gmail.com>
2024-12-10 10:43:37 -0300

    Translate legends to english and set GPU to auto

--- improve_images.sh
+++ improve_images.sh
@@ -16,49 +16,48 @@
 #   -f format            output image format (jpg/png/webp, default=ext/png)
 #   -v                   verbose output
 
-# Comprobamos que se hayan especificado todos los argumentos
+# We verify that all arguments are specified
 if [ "$#" -ne 4 ]; then
-  echo "Uso: $0 <carpeta_entrada> <carpeta_salida> <nombre_modelo> <procesar_subcarpetas/1-true/0-false (default)>"
-  echo "Ejemplo: $0 ./imagenes/originales ./imagenes/mejoradas 4 realesrgan-x4plus"
+  echo "Using: $0 <input_folder> <output_folder> <model_name> <process_subfolders/1-true/0-false (default)>"
+  echo "Example: $0 ./images/originals ./images/improved realesrgan-x4plus 1"
   exit 1
 fi
 
-# Asignamos los argumentos a variables para facilitar su uso
+# We assign the arguments to variables to facilitate their use
 carpeta_entrada="$1"
 carpeta_salida="$2"
 modelo="$3"
 subcarpetas="$4"
 
-# Creamos la carpeta de salida si no existe
 
 
-# Función para listar archivos en una carpeta
+# Function to list files in a folder
 procesar_archivos() {
     local current_directory="$1"    
-    #Obtenemos el nombre de la ultima carpeta de la ruta para crearla en el destino
+    # We get the name of the last route folder to create it at the destination
     local sub_folder_name=$(echo "$current_directory" | rev | cut -d'/' -f1 | rev)
-    echo "Procesando archivos en la carpeta: $current_directory"
+    echo "Processing files in the folder: $current_directory"
     local ruta_salida_final="$carpeta_salida/$sub_folder_name"
-    echo "Se enviaran archivos a la carpeta: $ruta_salida_final"        
+    echo "Files will be sent to the folder: $ruta_salida_final"        
     mkdir -p "$carpeta_salida"
     mkdir -p "$carpeta_salida/$sub_folder_name"
     for file in "$current_directory"/*; do
         if [ -f "$file" ]; then
-            # Obtenemos el nombre base del archivo (sin la ruta)
+            # We get the base name of the file (without the route)
             local file_name=$(basename "$file")
-            echo "nombre de archivo origen: $file"
-            # Definimos el archivo de salida en la carpeta de salida
+            echo "Original file name: $file"
+            # We define the output file in the output folder
             local archivo_salida="$ruta_salida_final/${file_name%.*}_mejorado.webp"
             local archivo_salida_webp="$ruta_salida_final/${file_name%.*}_mejoradoc.webp"            
-            echo "nombre de archivo de salida PNG: $archivo_salida"
-            echo "nombre de archivo de salida WEBP: $archivo_salida_webp"
-            ./realesrgan-ncnn-vulkan -i "$file" -o "$archivo_salida" -n "$modelo" -g 2 -f webp
+            echo "Output file in PNG format: $archivo_salida"
+            echo "Output file in WEBP format: $archivo_salida_webp"
+            ./realesrgan-ncnn-vulkan -i "$file" -o "$archivo_salida" -n "$modelo" -f webp
             cwebp -q 80 "$archivo_salida" -o "$archivo_salida_webp"            
             if [ $? -eq 0 ]; then
-                echo "Procesado y borrando origen: $file_name -> $archivo_salida"
+                echo "Processed and erasing origin: $file_name -> $archivo_salida"
                 rm "$archivo_salida"
             else
-                echo "Error al procesar: $file_name"
+                echo "Error when processing: $file_name"
             fi
         fi
     done
@@ -68,16 +67,16 @@ procesar_archivos() {
 
 # Procesamos cada archivo en la raiz carpeta de entrada
 cantidad_archivos=$(ls -p "$carpeta_entrada" | grep -v / | wc -l)
-echo "Cantidad de archivos en la raiz: $cantidad_archivos"
+echo "Amount of files in the root: $cantidad_archivos"
 if [ $cantidad_archivos -ne 0 ]; then
     procesar_archivos "${carpeta_entrada}"
 fi
 
-# Procesamos cada archivo en cada subcarpeta si el parametro es 1 en subcarpetas
+# We process each file in each subfolder if the parameter is 1 in subfolders
 if [ $4 -eq 1 ]; then
     for directory in in "$carpeta_entrada"/*/; do    
         if [ -d "$directory" ]; then
-            echo "${directory%/}"  # Eliminar la barra diagonal final
+            echo "${directory%/}"  # Remove the final diagonal bar
             ruta="$(dirname "$directory")"            
             procesar_archivos "${directory%/}" "$ruta"
         fi
